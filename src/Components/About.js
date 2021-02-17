905f10cd0b0b76d6f3eceb9b71cfafd79361614e
bramschabbink@hotmail.com <bramschabbink@hotmail.com>
2021-02-17 14:03:59 +0100

    added about info card

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,22 +1,12 @@
 import React from "react";
 import "./About.css";
+import "./Cards.css";
 import gif from "../profile-gif2.gif";
 
 function About() {
   return (
     <>
       <div className="about">
-        <div class="card">
-          <div class="container">
-            <p>
-              My name is Bram, I'm a full stack developer with a curious,
-              creative and innovative mindset. I recently graduated from an
-              intensive 3 month academy at Codaisseur. On this website you can
-              checkout some projects I'm working on.
-            </p>
-          </div>
-        </div>
-
         <img
           src={gif}
           alt="gif"
@@ -26,7 +16,25 @@ function About() {
             window.open("https://github.com/brambrulant/pinscreen", "_blank");
           }}
         />
-        <div></div>
+        <div class="card">
+          <div class="container2">
+            <card
+              onClick={(e) => {
+                e.preventDefault();
+                window.open("https://www.linkedin.com/in/bramsch/", "_blank");
+              }}
+            >
+              <p className="readable-text">
+                My name is Bram, I'm a developer and musician. I recently
+                graduated from an intensive 3 month academy at Codaisseur. On
+                this website you can checkout some projects I'm working on. Feel
+                free to contact me on LinkedIn by clicking this card! If you're
+                interested in the animation above, click on the image to see the
+                code on Github
+              </p>
+            </card>
+          </div>
+        </div>
       </div>
     </>
   );
